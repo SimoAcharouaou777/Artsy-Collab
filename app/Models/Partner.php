db0d3a7958ceabe7ae35561943fc59cd5c298eec
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-07 16:53:33 +0100

    changing in partner model

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Project;
 
-class Partener extends Model
+class Partner extends Model
 {
     use HasFactory;
     protected $table = 'partners';
-
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+    ];
     public function projects()
     {
         $this->hasMany(Project::class);
