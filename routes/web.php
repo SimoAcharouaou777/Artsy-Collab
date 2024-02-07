bde150828fbb42d647809e7255b41f0e27760b09
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-07 16:55:12 +0100

    working with images

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PartnerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+Route::get('/showUsers',[DashboardController::class,'showUsers'])->name('showUsers');
+Route::get('/showPartners',[PartnerController::class,'index'])->name('showPartners');
+Route::post('/partnerStore',[PartnerController::class,'store'])->name('partner.store');
