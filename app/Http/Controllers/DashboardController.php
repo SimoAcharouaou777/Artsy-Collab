4755206244c97d48409600f4b283f01b45cb9b03
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-07 16:52:34 +0100

    added dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class DashboardController extends Controller
 {
     public function index()
     {
         return view('admin.projects');
     }
+    public function showUsers()
+    {
+        return view('admin.users');
+    }
+
 }
