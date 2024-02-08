08abc52997f2f68e33decbd4fe94914b9c841b00
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-08 17:15:19 +0100

    adding fillable to project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,19 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
 use App\Models\Partner;
 
 class Project extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table = 'projects';
-
+    protected $fillable = [
+        'title',
+        'description',
+        'requirements',
+        'image',
+        'status',
+        'partner_id',
+    ];
+    
     public function users()
     {
         return $this->belongsToMany(User::class);
     }
-    public function parteners()
+    public function partner()
     {
         return $this->belongsTo(Partner::class);
     }
