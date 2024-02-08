de3553b4e38b56dab11a9c12a0846f93146f561c
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-08 17:17:24 +0100

    fixing bugs

--- resources/views/admin/projects.blade.php
+++ resources/views/admin/projects.blade.php
@@ -31,13 +31,13 @@
                         </ul>
                         @endif
                       </div>
-                        <form id="addBookForm" action="" method="post">
+                        <form id="addprojectform" action="{{route('project.store')}}" method="POST" enctype="multipart/form-data">
                         
-                            @csrf
+                          @csrf
                         
                             <div class="mb-3">
-                                <label for="projectname" class="form-label">Project Name</label>
-                                <input type="text" class="form-control" id="projectname" name="projectname" required>
+                                <label for="title" class="form-label">Project Name</label>
+                                <input type="text" class="form-control" id="title" name="title" required>
                             </div>
                             <div class="mb-3">
                               <label for="description" class="form-label">Description</label>
@@ -50,15 +50,21 @@
                             </div>
         
                             <div class="mb-3">
-                                <label for="partner" class="form-label">Partner</label>
-                                <input type="text" class="form-control" id="description" name="description" required>
+                              <label for="partner" class="form-label">Partner</label>
+                              <select class="form-control" id="partner" name="partner" required>
+                                  <option value="" disabled selected>Select a partner</option>
+                                  @foreach($partners as $partner)
+                                      <option value="{{ $partner->id }}">{{ $partner->name }}</option>
+                                  @endforeach
+                              </select>
                             </div>
+                          
 
                             <div class="mb-3">
                               <label for="image" class="form-label">Image</label>
                               <input type="file" class="form-control" id="image" name="image" accept="image/*" required>
                             </div>
-                            <button type="submit" class="btn btn-primary" name="submit">ADD</button>
+                            <button type="submit" class="btn btn-primary" >ADD</button>
                         </form>
                     </div>
                   
@@ -80,6 +86,7 @@
             <th>Project Name </th>
             <th>Description</th>
             <th>Requirements</th>
+            <th>Project Status</th>
             <th>Partner Name</th>
             <th>Number Of Collaboraters</th>
             <th>Project Image</th>
@@ -90,21 +97,40 @@
         <tbody id="category">
 
 
-          {{-- @foreach --}}
+          @foreach($projects as $project)
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>{{$project->id}}</td>
+                <td>{{$project->title}}</td>
+                <td>{{$project->description}}</td>
+                <td>{{$project->requirements}}</td>
+                <td>{{$project->status}}</td>
+                <td>{{$project->partner->name}}</td>
+                <td>number of coll</td>
+                <td><img src="{{ asset('storage/' . $project->image) }}" alt="project Image" style="max-width: 70px;"></td>
 
        
                 <td class="d-flex gap-2">
-                <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editModal" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"> <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/> </svg></button>
-                
-                <form action="" method="post" >
+                <form action="{{ route('project.updateStatus', $project->id) }}" method="post">
+                  @csrf
+                  @method('PUT')
+                  <button type="submit" class="btn btn-light" name="status" value="published">
+                    <svg class="bi bi-check-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" viewBox="0 0 16 16">
+                      <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM7 11a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0v5a.5.5 0 0 1 0 1zM8 5.5a.5.5 0 0 1 1 0V10a.5.5 0 0 1-1 0V5.5zM4 8a1 1 0 0 1 1-1h5a1 1 0 0 1 0 2H5a1 1 0 0 1-1-1z"/>
+                    </svg>
+                  </button>
+              </form>
+
+              <form action="{{ route('project.updateStatus', $project->id) }}" method="post">
+                @csrf
+                @method('PUT')
+                <button type="submit" class="btn btn-light" name="status" value="unpublished">
+                  <svg class="bi bi-x-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" viewBox="0 0 16 16">
+                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM5.354 4.646a.5.5 0 0 1 .708 0L8 6.293l2.938-2.939a.5.5 0 0 1 .707.708L8.707 7l2.938 2.938a.5.5 0 0 1-.707.708L8 7.707 5.354 10.354a.5.5 0 0 1-.708-.708L7.293 7 4.646 4.354a.5.5 0 0 1 0-.708z"/>
+                  </svg>  
+                </button>
+            </form>
+
+                <form action="{{route('project.sofdelete' , $project->id)}}" method="post" >
                     @csrf 
                     @method('DELETE')
                     <button type="submit" name="submit"  class="btn btn-light">
@@ -113,7 +139,7 @@
                 </form>
                 </td>
             </tr>
-            {{-- @endforeach --}}
+            @endforeach
 
 
                {{-- Edite Modal  --}}
