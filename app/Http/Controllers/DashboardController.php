4a0c700256c1e853e1f40a6de962759c8492f75d
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-08 17:13:43 +0100

    separating the usercontroller from dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Partner;
+use App\Models\Project;
 
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.projects');
+        $partners = Partner::all();
+        $projects = Project::with('partner')->get();
+        return view('admin.projects',compact('partners','projects'));
     }
-    public function showUsers()
-    {
-        return view('admin.users');
-    }
+ 
 
 }
