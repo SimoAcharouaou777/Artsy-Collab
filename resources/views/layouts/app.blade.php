cfe8f18951517501495b4407d75b35e6bf6ad7f7
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-09 22:02:54 +0100

    adding asset for fixing styling errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,26 +12,27 @@
 
   <title>{{ config('app.name', 'Laravel') }}</title>
    <!--boostrap icons -->
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href={{asset("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css")}}>
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href={{asset("assets/img/favicon.png")}} rel="icon">
+  <link href={{asset("assets/img/apple-touch-icon.png")}} rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href={{asset("https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i")}} rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset("assets/vendor/aos/aos.css")}}">
+  <link href={{asset("assets/vendor/aos/aos.css")}} rel="stylesheet">
+  <link href={{asset("assets/vendor/bootstrap/css/bootstrap.min.css")}} rel="stylesheet">
+  <link href={{asset("assets/vendor/bootstrap-icons/bootstrap-icons.css" )}}rel="stylesheet">
+  <link href={{asset("assets/vendor/boxicons/css/boxicons.min.css")}} rel="stylesheet">
+  <link href={{asset("assets/vendor/glightbox/css/glightbox.min.css")}} rel="stylesheet">
+  <link href={{asset("assets/vendor/remixicon/remixicon.css")}} rel="stylesheet">
+  <link href={{asset("assets/vendor/swiper/swiper-bundle.min.css")}} rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href={{asset("assets/css/style.css")}} rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Arsha
@@ -48,17 +49,17 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
+      <h1 class="logo me-auto"><a href={{asset("index.html")}}>Arsha</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto active" href={{asset("#hero")}}>Home</a></li>
+          <li><a class="nav-link scrollto" href={{asset("#about")}}>About</a></li>
+          <li><a class="nav-link scrollto" href={{asset("#services")}}>Services</a></li>
+          <li><a class="nav-link   scrollto" href={{asset("#portfolio")}}>Portfolio</a></li>
+          <li><a class="nav-link scrollto" href={{asset("#team")}}>Team</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
@@ -188,21 +189,21 @@
     </div>
   </footer><!-- End Footer -->
 
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  {{-- <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> --}}
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src={{asset("assets/vendor/aos/aos.js")}}></script>
+  <script src={{asset("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")}}></script>
+  <script src={{asset("assets/vendor/glightbox/js/glightbox.min.js")}}></script>
+  <script src={{asset("assets/vendor/isotope-layout/isotope.pkgd.min.js")}}></script>
+  <script src={{asset("assets/vendor/swiper/swiper-bundle.min.js")}}></script>
+  <script src={{asset("assets/vendor/waypoints/noframework.waypoints.js")}}></script>
+  <script src={{asset("assets/vendor/php-email-form/validate.js")}}></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src={{asset("assets/js/main.js")}}></script>
+  <script src={{asset("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js")}}></script>
 </body>
 
 </html>
