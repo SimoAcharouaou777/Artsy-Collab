21da4de0ed39710d6331bd8226cca72f18487b35
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-09 15:54:06 +0100

    adding on delete cascade to the foreignId

--- database/migrations/2024_02_08_092954_add_partner_id_to_projects_table.php
+++ database/migrations/2024_02_08_092954_add_partner_id_to_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->foreignId('partner_id')->constrained('partners');
+            $table->foreignId('partner_id')->constrained('partners')->onDelete('cascade');
         });
     }
 
