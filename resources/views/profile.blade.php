64b6d63f2986d83d211ec27999d83530a2734c5f
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-12 17:27:31 +0100

    creating user profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('content')
+<div class="container-xl px-4 mt-4">
+    <nav class="nav nav-borders">
+        <a class="nav-link active ms-0" href="https://www.bootdey.com/snippets/view/bs5-edit-profile-account-details" target="__blank">Profile</a>
+        <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-profile-billing-page" target="__blank">Billing</a>
+        <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-profile-security-page" target="__blank">Security</a>
+        <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-edit-notifications-page"  target="__blank">Notifications</a>
+    </nav>
+    <hr class="mt-0 mb-4">
+    <div class="row">
+        <div class="col-xl-4">
+            <!-- Profile picture card-->
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Profile Picture</div>
+                <div class="card-body text-center">
+                    <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                    <button class="btn btn-primary" type="button">Upload new image</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-8">
+            <!-- Account details card-->
+            <div class="card mb-4">
+                <div class="card-header">Account Details</div>
+                <div class="card-body">
+                    
+                    <form action="{{ route('profile.update', Auth::user()->id) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputUsername"></label>
+                            <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="{{ Auth::user()->username }}">
+                        </div>
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
+                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="{{ Auth::user()->email }}">
+                        </div>
+                        <div class="mb-3">
+                            <label class="small mb-1" for="skills">Skills</label>
+                            <input class="form-control" id="skills" type="skills" placeholder="Enter Your Skills ex : artist , voice-over" value="{{Auth::user()->skills}}">
+                        </div>
+                        <button class="btn btn-primary" type="submite">Save changes</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
