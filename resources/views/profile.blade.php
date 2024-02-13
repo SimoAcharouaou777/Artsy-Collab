dfbae693c0ea9fe8a100c87f7781be062c2dc9c6
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-13 11:26:42 +0100

    displaying the user profile whenerver he updates it in hes profile section

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,15 +8,18 @@
         <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-edit-notifications-page"  target="__blank">Notifications</a>
     </nav>
     <hr class="mt-0 mb-4">
+    <form action="{{ route('profile.update', Auth::user()->id) }}" method="post"  enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
     <div class="row">
         <div class="col-xl-4">
             <!-- Profile picture card-->
             <div class="card mb-4 mb-xl-0">
                 <div class="card-header">Profile Picture</div>
                 <div class="card-body text-center">
-                    <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                    <img class="img-account-profile rounded-circle mb-2" src="{{ asset('storage/' . Auth::user()->profile) }}" alt="" style="width: 300px;">
                     <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                    <button class="btn btn-primary" type="button">Upload new image</button>
+                        <input type="file" class="form-control" id="profile_picture" name="profile_picture" accept="image/png, image/jpeg">
                 </div>
             </div>
         </div>
@@ -25,10 +28,6 @@
             <div class="card mb-4">
                 <div class="card-header">Account Details</div>
                 <div class="card-body">
-                    
-                    <form action="{{ route('profile.update', Auth::user()->id) }}" method="post">
-                        @csrf
-                        @method('PUT')
                         <div class="mb-3">
                             <label class="small mb-1" for="username"></label>
                             <input class="form-control" name="username" id="username" type="text" placeholder="Enter your username" value="{{ Auth::user()->username }}">
@@ -41,12 +40,11 @@
                             <label class="small mb-1" for="skills">Skills</label>
                             <input class="form-control" name="skills" id="skills" type="skills" placeholder="Enter Your Skills ex : artist , voice-over" value="{{Auth::user()->skills}}">
                         </div>
-                        <button class="btn btn-primary" type="submite">Save changes</button>
-                    </form>
-                    
+                        <button class="btn btn-primary" type="submit">Save changes</button>
                 </div>
             </div>
         </div>
     </div>
+     </form>
 </div>
 @endsection
