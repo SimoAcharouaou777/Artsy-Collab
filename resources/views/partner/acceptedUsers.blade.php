cb71283e362975b6ae4a4145a0ee105b8fa1d833
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-14 22:34:48 +0100

    fixing bugs

--- resources/views/partner/acceptedUsers.blade.php
+++ resources/views/partner/acceptedUsers.blade.php
@@ -96,48 +96,52 @@
         <tbody id="category">
 
 
-          @foreach($acceptedUsers as $collab)
-            <tr>
-                <td>{{ $collab->title }}</td>
-                <td><img src="{{ asset('storage/' . $collab->image) }}" alt="Project Image" style="max-width: 70px;"></td>
-                <td>the number</td>
-                <td>{{ $collab->users->first()->username }}</td>
-                <td>{{ $collab->users->first()->skills }}</td>
-                <td>{{ $collab->requirements }}</td>
-                <td>{{ $collab->pivot->status }}</td>
+          @foreach($pendingUsers as $user)
+          @foreach($user->projects as $collab)
+       <tr>
+         <td>{{ $collab->title }}</td>
+         <td><img src="{{ asset('storage/' . $collab->image) }}" alt="Project Image" style="max-width: 70px;"></td>
+         <td>the number</td>
+         <td>{{ $user->username }}</td>
+         <td>{{ $user->skills }}</td>
+         <td>{{ $collab->requirements }}</td>
+         <td>{{ $collab->pivot->status }}</td>
+  
+           <td class="d-flex gap-2">
+           @php
+             $route = $collab->id."-".$user->id;
+           @endphp
+           <form action="{{route('acceptUser.Request', $route)}}" method="post">
+             @csrf
+             @method('PUT')
+             <button type="submit" class="btn btn-light" name="status" value="accepted">
+               <svg class="bi bi-check-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" viewBox="0 0 16 16">
+                 <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM7 11a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0v5a.5.5 0 0 1 0 1zM8 5.5a.5.5 0 0 1 1 0V10a.5.5 0 0 1-1 0V5.5zM4 8a1 1 0 0 1 1-1h5a1 1 0 0 1 0 2H5a1 1 0 0 1-1-1z"/>
+               </svg>
+             </button>
+         </form>
 
-       
-                <td class="d-flex gap-2">
-                <form action="{{route('acceptUser.Request', $collab->id)}}" method="post">
-                  @csrf
-                  @method('PUT')
-                  <button type="submit" class="btn btn-light" name="status" value="accepted">
-                    <svg class="bi bi-check-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" viewBox="0 0 16 16">
-                      <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM7 11a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0v5a.5.5 0 0 1 0 1zM8 5.5a.5.5 0 0 1 1 0V10a.5.5 0 0 1-1 0V5.5zM4 8a1 1 0 0 1 1-1h5a1 1 0 0 1 0 2H5a1 1 0 0 1-1-1z"/>
-                    </svg>
-                  </button>
-              </form>
+         <form action="{{route('acceptUser.Request', $route)}}" method="post">
+           @csrf
+           @method('PUT')
+           <button type="submit" class="btn btn-light" name="status" value="refused">
+             <svg class="bi bi-x-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" viewBox="0 0 16 16">
+               <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM5.354 4.646a.5.5 0 0 1 .708 0L8 6.293l2.938-2.939a.5.5 0 0 1 .707.708L8.707 7l2.938 2.938a.5.5 0 0 1-.707.708L8 7.707 5.354 10.354a.5.5 0 0 1-.708-.708L7.293 7 4.646 4.354a.5.5 0 0 1 0-.708z"/>
+             </svg>  
+           </button>
+       </form>
 
-              <form action="{{route('refuseUser.Request', $collab->id)}}" method="post">
-                @csrf
-                @method('PUT')
-                <button type="submit" class="btn btn-light" name="status" value="refused">
-                  <svg class="bi bi-x-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" viewBox="0 0 16 16">
-                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM5.354 4.646a.5.5 0 0 1 .708 0L8 6.293l2.938-2.939a.5.5 0 0 1 .707.708L8.707 7l2.938 2.938a.5.5 0 0 1-.707.708L8 7.707 5.354 10.354a.5.5 0 0 1-.708-.708L7.293 7 4.646 4.354a.5.5 0 0 1 0-.708z"/>
-                  </svg>  
-                </button>
-            </form>
-
-                <form action="" method="post" >
-                    @csrf 
-                    @method('DELETE')
-                    <button type="submit" name="submit"  class="btn btn-light">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>
-                    </button>
-                </form>
-                </td>
-            </tr>
-            @endforeach
+           <form action="" method="post" >
+               @csrf 
+               @method('DELETE')
+               <button type="submit" name="submit"  class="btn btn-light">
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>
+               </button>
+           </form>
+           </td>
+       </tr>
+       @endforeach
+           @endforeach
 
 
                {{-- Edite Modal  --}}
