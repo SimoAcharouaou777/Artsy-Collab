977de35942281f4d11e5bb5f6727460ef7f45031
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-14 22:34:33 +0100

    changes the view of banned user

--- resources/views/ban/banned.blade.php
+++ resources/views/ban/banned.blade.php
@@ -1,1 +1,9 @@
-you are banned
+<style>
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover; 
+    }
+</style>
+
+<img src="{{ asset('images/téléchargement.jpg') }}" alt="Your Image">
