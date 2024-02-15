e802a28a45eee8db3c48756d3706291b82799fed
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-15 14:47:47 +0100

    changing the view for the admin

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -123,9 +123,10 @@
                   @method('PUT')
               
                   <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editUserRoleModal{{ $user->id }}">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> 
-                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                      <path d="M14.207 1.293a1 1 0 0 1 0 1.414L3.414 14.207a1 1 0 0 1-1.414-1.414L12.793 1.293a1 1 0 0 1 1.414 0zM3 13V15h2l9-9-2-2L3 13z"/>
+                      <path fill-rule="evenodd" d="M15.854 2.146a.5.5 0 0 0 0-.708l-1-1a.5.5 0 0 0-.708 0L13 1.293 14.707 3l1-1a.5.5 0 0 0 0-.708zM12 4l1.5-1.5L2 14v1h1L12 4z"/>
+                  </svg>
                   </button>
               </form>
                 </td>
